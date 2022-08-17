b00e8a20360868a7eaf046cbdf9d25a40a2bebc8
Wojciech Trocki <wtrocki@redhat.com>
2022-08-17 21:42:47 +0100

    fix: initial version of templates

---
+++ scripts/generate.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# generates an API client for the given OpenAPI spec file
+generate_sdk() {
+    local oas_file_name=$1
+    local output_path=$2
+    local package_name=$3
+     
+    # echo "Validating OpenAPI ${oas_file_name}"
+    # npx @openapitools/openapi-generator-cli validate -i "$oas_file_name"
+
+    echo "Generating source code based on ${oas_file_name}"
+
+    # remove old generated models
+    rm -Rf $OUTPUT_PATH
+
+    echo "Generating SDK"
+    npx @openapitools/openapi-generator-cli generate -g python -i\
+        $oas_file_name -o $output_path \
+        --package-name="$package_name" \
+        --ignore-file-override=.openapi-generator-ignore \
+        --template-dir _templates/python \
+        --package-name=$package_name
+
+}
+
+OPENAPI_FILENAME="openapi/kas-fleet-manager.yaml"
+PACKAGE_NAME="rhoas_kafka_mgmt_sdk"
+OUTPUT_PATH="test/kafka_mgmt_sdk"
+
+generate_sdk $OPENAPI_FILENAME $OUTPUT_PATH $PACKAGE_NAME
